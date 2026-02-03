22c84ac596f3bba1f3dd657053948486e284abb0
jamalgossi2005-art <jamalgossi2005@gmail.com>
2026-02-03 22:31:17 +0100

    main.py

--- P1/P1C4/énoncé/main.py
+++ P1/P1C4/énoncé/main.py
@@ -1,1 +1,8 @@
-# Écrivez votre code ici !
+# declaration d'age
+age=20
+# declaration de nom 
+nom="itachi uchiha"
+print(f"je m'appele {nom} et j'ai {age}")
+#ajouter 10ans a l'age acteul 
+age=age+10
+print(f"je m'appele {nom} et j'ai {age}")
