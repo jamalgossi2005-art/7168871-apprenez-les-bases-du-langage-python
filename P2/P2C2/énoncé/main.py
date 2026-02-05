af8b6da0a67570668d4f20795fa7b208120847e2
jamalgossi2005-art <jamalgossi2005@gmail.com>
2026-02-06 00:12:09 +0100

    gerer_la_classe.py
    Implement note addition and validation logic.

--- P2/P2C2/énoncé/main.py
+++ P2/P2C2/énoncé/main.py
@@ -1,1 +1,19 @@
-# Ecrivez votre code ici !
+liste=[]
+listevalider=[]
+add=True
+print("ajouter une note")
+while add :
+    liste.append(int(input("entrer une note")))
+    if not input("est ce que tu veux ajouter une note ? ")=='oui' :
+      add=False
+somme=0
+for x in liste :
+   somme+=x
+   if x>=10 :
+      listevalider.append(int(x))
+for x in range(len(listevalider)) :
+ if x==0 :
+  print(f" somme    moyenne     les notes valider ")
+  print(f" {somme}        {round(somme/2,3)}            {listevalider[x]} ")
+ else :
+  print(f"                            {listevalider[x]}    ")# Ecrivez votre code ici !
