92480cab377720db637bfd6ccf8017139e8a12bd
jamalgossi2005-art <jamalgossi2005@gmail.com>
2026-02-06 22:27:12 +0100

    main.py

--- P3/P3C1/énoncé/main.py
+++ P3/P3C1/énoncé/main.py
@@ -1,1 +1,4 @@
-# Ecrivez votre code ici !
+import packagejamal.calculatorFunctions 
+if __name__=='__main__':
+ print(packagejamal.calculatorFunctions.somme(3,5))
+ print(packagejamal.calculatorFunctions.produit(3,5))
