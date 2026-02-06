31a765c587423c70f6a1a44e6c855b7f44b371d6
jamalgossi2005-art <jamalgossi2005@gmail.com>
2026-02-06 19:26:50 +0100

    salaire.py

--- P2/P2C3/énoncé/main.py
+++ P2/P2C3/énoncé/main.py
@@ -1,1 +1,10 @@
-# Ecrivez votre code ici
+def salaire_mensuel(slaire_A) :
+    return slaire_A/12
+def salaire_hebdomadaire(salire_M) :
+    return salire_M/4 
+def salaire_horaire(salaire_H , nbreH) :
+      return salaire_H/nbreH
+salire_anneul=int(input("donner votre salaire annuel : "))
+#HT=nombres des hueres
+HT=int(input("donner le nombre des heures que vous travaillez dans chaque semaine"))
+print(f"votre salaire horaire est : {round(salaire_horaire(salaire_hebdomadaire(salaire_mensuel(salire_anneul)),HT))} ")
