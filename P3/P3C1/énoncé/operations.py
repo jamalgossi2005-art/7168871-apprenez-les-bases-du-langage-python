ce6d070af02543acdf4873dea7c110b434420af9
jamalgossi2005-art <jamalgossi2005@gmail.com>
2026-02-06 22:29:44 +0100

    Implement basic arithmetic operations in operations.py

--- P3/P3C1/énoncé/operations.py
+++ P3/P3C1/énoncé/operations.py
@@ -1,1 +1,16 @@
-# Écrivez votre code ici !
+# la somme 
+def somme(num1 , num2):
+    return num1+num2
+#le produit
+def produit(num1 , num2):
+    return num1*num2
+#la division
+def division(num1 , num2):
+    try:
+     return num1/num2
+    except ZeroDivisionError:
+       print("impossible de diviser par zero ")
+#la soustraction      
+def soustraction(num1 , num2) :
+     return num1-num2
+
