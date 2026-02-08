b0391664ad0cd0c3d07fb3e97f22e6d0fb4b7410
jamalgossi2005-art <jamalgossi2005@gmail.com>
2026-02-08 17:01:46 +0100

    Implement CSV read and write functionality.py
    Added functions to read from 'input.csv' and write to 'output.csv'.

--- P3/P3C3/énoncé/main.py
+++ P3/P3C3/énoncé/main.py
@@ -1,6 +1,28 @@
 # Écrivez votre code ici !
 
+import csv 
+liste1=[]
+liste2=[]
+en_tete=["nom","heures_travaillees"]
+def    read():
+        with open("input.csv", "r") as file_r :
+         reader=csv.DictReader(file_r )
+         for line in reader :
+          liste1.append(line['nom'])
+          liste2.append(str(float(line['heures_travaillees'])*15))
+          
+def   write():
+           
+        with open("output.csv" , "w") as file_w :
+         writer=csv.writer(file_w , delimiter=',')
+         writer.writerow(en_tete) 
+         for nom , heure in zip (liste1,liste2) :
+               liste=[nom,heure]
+               writer.writerow(liste)
+        
+read()
 
+write()
 # Ne touchez pas le code ci-dessous
 if __name__ == "__main__":
     main()
